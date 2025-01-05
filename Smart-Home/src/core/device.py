2b73c02133601a2b32a4268f0729e762d3ea1388
Maks Zaikin <Max.V.Zaikin@gmail.com>
2025-01-06 00:05:58 +1100

    feat: Abstract device class added

---
+++ Smart-Home/src/core/device.py
@@ -0,0 +1,18 @@
+""" 
+Project Smart-House
+
+Author: Maks V. Zaikin
+Date: 06-Jan-2025
+"""
+
+from abc import ABC, abstractmethod
+
+class Device(ABC):
+    """Abstract device class"""
+    @abstractmethod
+    def update(self):
+        """update Updates device's state
+
+        _extended_summary_
+        """
+        pass
